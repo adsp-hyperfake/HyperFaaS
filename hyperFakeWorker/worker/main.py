6d0c3bf34f3a568d73c6fc6596461cd606fbc9b8
Leo Wilhelm Lierse <lierseleow@gmail.com>
2025-06-04 07:09:08 +0200

    added subcommand to check function instances

--- hyperFakeWorker/worker/main.py
+++ hyperFakeWorker/worker/main.py
@@ -18,12 +18,25 @@ def server():
 def client():
     import grpc
     from .api.controller.controller_pb2_grpc import ControllerStub
-    from .api.controller.controller_pb2 import StatusRequest
+    from .api.controller.controller_pb2 import StateRequest, StateResponse
+    from .api.common.common_pb2 import FunctionID, InstanceID, CallRequest, CallResponse
     channel = grpc.insecure_channel("localhost:50051")
     stub = ControllerStub(channel)
 
-    status_future = stub.Status(StatusRequest(nodeID="0"))
-    status_future.result()
+    state: StateResponse = stub.State(StateRequest(node_id="12345"))
+    for function in state.functions:
+        print(f"State of functions: {function.function_id}")
+        print(f"Checking {len(function.idle)} idle function instances:")
+        for func in function.idle:
+            print(f"Checking instance {func.instance_id}: {func}")
+            call_future = stub.Call(CallRequest(instance_id=InstanceID(id=func.instance_id), function_id=function.function_id))
+            print(call_future)
+        print(f"Checking {len(function.running)} running function instances:")
+        for func in function.running:
+            print(f"Checking instance {func.instance_id}: {func}")
+            call_future = stub.Call(CallRequest(instance_id=InstanceID(id=func.instance_id), function_id=function.function_id))
+            print(call_future)
+    
 
 
 if __name__ == "__main__":
