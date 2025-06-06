f7687134ed5836a7c22cdc7b7b97839781ecec96
Leo Wilhelm Lierse <lierseleow@gmail.com>
2025-06-06 14:37:58 +0200

    trying to fix leaf corrupting error

--- hyperFakeWorker/worker/log.py
+++ hyperFakeWorker/worker/log.py
@@ -3,4 +3,4 @@ import logging
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(asctime)s - %(module)s: %(message)s")
 
 logger = logging.getLogger(None)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
